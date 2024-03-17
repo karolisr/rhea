f58a8acc984264c126d7251ca6604f728612b478
Karolis Ramanauskas <karolisr@gmail.com>
2024-03-16 21:06:22 -0500

    Basic feature annotation SVG.

--- src/routes/view/[recid]/SeqView.svelte
+++ src/routes/view/[recid]/SeqView.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 const nums: number[] = []
-const m = 32
+const m = 10
 
 for (let i = 1; i < m; i++) {
   nums.push((i / m) * 100)
@@ -8,13 +8,20 @@ for (let i = 1; i < m; i++) {
 </script>
 
 <div class="border-0 border-solid border-neutral-950">
-  <svg class="">
-    {#each nums as i}
-      <g class="stroke-neutral-400">
-        <line x1="0%" x2="100%" y1="{i}%" y2="{i}%"></line>
-        <line y1="0%" y2="100%" x1="{i}%" x2="{i}%"></line>
-      </g>
-    {/each}
+  <svg viewBox="0 0 640 640" preserveAspectRatio="none">
+    <g class="bg">
+      <rect x="0%" y="0%" width="100%" height="100%"></rect>
+      {#each nums as i}
+        <line x1="0%" y1="{i}%" x2="100%" y2="{i}%"></line>
+        <line x1="{i}%" y1="0%" x2="{i}%" y2="100%"></line>
+      {/each}
+    </g>
+    <g transform="translate(0, 0)">
+      <polyline points="0 0, 480 0, 640 320, 480 640, 0 640, 0 0"></polyline>
+      <!-- <g transform="translate(375, 500)">
+        <text font-weight="bold">Feature</text>
+      </g> -->
+    </g>
   </svg>
 </div>
 
@@ -31,11 +38,29 @@ svg {
   margin: 0px;
   width: 640px;
   height: 640px;
-  /* background-color: lemonchiffon; */
-  background-color: cornsilk;
+  background-color: transparent;
   stroke: black;
-  stroke-width: 1px;
-  /* stroke-linecap: round; */
-  /* stroke-linejoin: round; */
+  fill: floralwhite;
+  /* stroke-width: 1px; */
+}
+
+svg g {
+  align-items: center;
+  text-align: center;
+}
+
+svg text {
+  font-family: 'Courier New', Courier, monospace;
+  stroke: transparent;
+  stroke-width: 0px;
+  fill: black;
+  text-anchor: middle;
+  dominant-baseline: middle;
+  font-size: 100px;
+}
+
+svg .bg {
+  stroke: #a3a3a3;
+  fill: cornsilk;
 }
 </style>
