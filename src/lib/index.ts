a9bfa6719464106d0decdb9ba1101afa05b36f76
Karolis Ramanauskas <karolisr@gmail.com>
2024-03-24 12:17:31 -0500

    Adds a "removeCycle" function to get rid of cyclic object structures.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -5,3 +5,14 @@ export function getPropNames<T>(obj: T) {
 export function setDiff<T>(a: Set<T>, b: Set<T>) {
   return new Set([...a].filter((x) => !b.has(x)))
 }
+
+export function removeCycle(obj: object, stack: object[] = []): object | string {
+  if (!obj || typeof obj !== 'object') return obj
+  if (stack.includes(obj)) return 'CYCLIC'
+  let s = stack.concat([obj])
+  return Array.isArray(obj)
+    ? obj.map((x) => removeCycle(x, s))
+    : Object.fromEntries(
+        Object.entries(obj).map(([k, v]) => [k, removeCycle(v, s)])
+      )
+}
