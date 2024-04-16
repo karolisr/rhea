1b6ee04d0782b496c819ccbbfa00355da2133fa4
Karolis Ramanauskas <karolisr@gmail.com>
2024-04-16 16:27:27 -0500

    Start CollectionView.

---
+++ src/routes/collections/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+import { onMount } from 'svelte'
+import { type Readable } from 'svelte/store'
+import { type DBMainSvelteStore } from '$lib/app/svelte-stores/db-main'
+import db_main from '$lib/app/svelte-stores/db-main'
+
+let _db_main: Readable<DBMainSvelteStore>
+$: collections = $_db_main?.collection
+
+onMount(async () => {
+  _db_main = await db_main
+  collections = $_db_main?.collection
+})
+</script>
+
+<div class="OUTER">
+  <div class="INNER-L"></div>
+  <div class="INNER-R"></div>
+</div>
+
+<style lang="scss">
+.OUTER {
+  background-color: aliceblue;
+  flex-grow: 1;
+  display: grid;
+  grid-template-columns: 280px 1fr;
+}
+
+.INNER-L {
+  background-color: beige;
+  border-right-style: solid;
+}
+
+.INNER-R {
+  background-color: bisque;
+}
+</style>
--- src/routes/nav.svelte
+++ src/routes/nav.svelte
@@ -4,7 +4,7 @@ import TabSpacer from '$lib/ui/chrome/nav/components/tabnav/TabSpacer.svelte'
 import Tab from '$lib/ui/chrome/nav/components/tabnav/Tab.svelte'
 
 import iconHome from '~icons/fa6-solid/house-chimney'
-// import iconCollections from '~icons/fa6-solid/database'
+import iconCollections from '~icons/fa6-solid/database'
 // import iconSearch from '~icons/fa6-solid/magnifying-glass'
 import iconSettings from '~icons/fa6-solid/gear'
 import iconDebug from '~icons/fa6-solid/bug'
@@ -13,8 +13,8 @@ import iconDebug from '~icons/fa6-solid/bug'
 <TabNav uid="nav-main">
   <TabSpacer uid="nav-main-left-spacer" minW="{68}" collapsable />
   <Tab link="/" icon="{iconHome}" label="Home" />
-  <!-- <Tab link="/collections/" icon="{iconCollections}" label="Collections" /> -->
+  <Tab link="/collections/" icon="{iconCollections}" label="Collections" />
   <!-- <Tab link="/search/" icon="{iconSearch}" label="Search" /> -->
-  <Tab link="/settings/" icon="{iconSettings}" label="Settings" />
   <Tab link="/debug/" icon="{iconDebug}" label="Debug" current="startsWith" />
+  <Tab link="/settings/" icon="{iconSettings}" label="Settings" />
 </TabNav>
