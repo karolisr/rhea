adfebfaed587abb15830fdf097bf82675d5848b5
Karolis Ramanauskas <karolisr@gmail.com>
2024-04-29 17:31:28 -0500

    DB functions for Collections.

--- src/lib/app/svelte-stores/db-main.ts
+++ src/lib/app/svelte-stores/db-main.ts
@@ -8,7 +8,7 @@ import {
   db_del,
   db_get_all_from_index
 } from '$lib/app/db'
-import type { DBMain, Collection } from '$lib/app/db/types'
+import type { DBMain, Collection, CollectionMapping } from '$lib/app/db/types'
 import type {
   IDBPDatabase,
   StoreNames,
@@ -32,11 +32,13 @@ export interface DBMainSvelteStore {
   del: typeof db_main_del
   createCollection: typeof createCollection
   deleteCollection: typeof deleteCollection
+  relabelCollection: typeof relabelCollection
   get_all_from_index: typeof db_main_get_all_from_index
   seq_nt_summ: ESummaryNuccore[]
   taxon: Taxon[]
   gbseq: GBSeq[]
   collection: Collection[]
+  coll_gbseq_map: CollectionMapping[]
   db: IDBPDatabase<DBMain>
 }
 
@@ -62,11 +64,13 @@ async function prep_db_main() {
     del: db_main_del,
     createCollection: createCollection,
     deleteCollection: deleteCollection,
+    relabelCollection: relabelCollection,
     get_all_from_index: db_main_get_all_from_index,
     gbseq: (await db_main_get_all('gbseq')) ?? [],
     seq_nt_summ: (await db_main_get_all('seq_nt_summ')) ?? [],
     taxon: (await db_main_get_all('taxon')) ?? [],
     collection: (await db_main_get_all('collection')) ?? [],
+    coll_gbseq_map: [],
     db
   }
   return writable(_db_main_svelte_store)
@@ -105,7 +109,7 @@ const db_main_put = async <StoreName extends StoreNames<DBMain>>(
   store_name: StoreName
 ) => {
   await db_put(items, store_name, db)
-  await updateStore()
+  updateStore(store_name)
 }
 
 const db_main_del = async <StoreName extends StoreNames<DBMain>>(
@@ -113,7 +117,7 @@ const db_main_del = async <StoreName extends StoreNames<DBMain>>(
   store_name: StoreName
 ) => {
   await db_del(id, store_name, db)
-  await updateStore()
+  await updateStore(store_name)
 }
 
 const createCollection = async (
@@ -123,25 +127,43 @@ const createCollection = async (
 ) => {
   const _ = { id: uuid(), parentId, label, notes }
   await db_main_put([_], 'collection')
+  return (await db_main_get(_.id, 'collection')).id
 }
 
 const deleteCollection = async (id: unknown) => {
   if (id !== 'ROOT') {
     await db_main_del(id as string, 'collection')
+    await pruneCollections()
   }
 }
 
-async function updateStore() {
-  const gbseq = await db_main_get_all('gbseq')
-  const seq_nt_summ = await db_main_get_all('seq_nt_summ')
-  const taxon = await db_main_get_all('taxon')
-  const collection = await db_main_get_all('collection')
+const pruneCollections = async () => {
+  const obs = await db.getAll('collection')
+  const ids = await db.getAllKeys('collection')
+  const _: string[] = []
+  for (let i = 0; i < obs.length; i++) {
+    const ob = obs[i]
+    const pid = ob.parentId
+    if (pid !== 'NONE' && !ids.includes(pid)) {
+      _.push(ob.id)
+      await db_main_del(ob.id, 'collection')
+    }
+  }
+}
+
+const relabelCollection = async (id: string, label: string) => {
+  const _ = await db_main_get(id, 'collection')
+  _.label = label
+  await db_main_put([_], 'collection')
+}
+
+async function updateStore<StoreName extends StoreNames<DBMain>>(
+  store_name: StoreName
+) {
+  const val = await db_main_get_all(store_name)
   const store = await db_main
   store.update((_) => {
-    _.gbseq = gbseq
-    _.seq_nt_summ = seq_nt_summ
-    _.taxon = taxon
-    _.collection = collection
+    _[store_name] = val as DBMainSvelteStore[StoreName]
     return _
   })
 }
