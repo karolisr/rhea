5ada6ff1998fc6e87b2ecfa29d69057eaf2670f1
Karolis Ramanauskas <karolisr@gmail.com>
2024-05-10 16:50:33 -0500

    FileDrop.svelte fix.

--- src/lib/ui/FileDrop.svelte
+++ src/lib/ui/FileDrop.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 import { onMount, onDestroy } from 'svelte'
-import { fileDropListener } from '$lib/app/api/filedrop'
+import { dragDropFileListener } from '$lib/app/api/drag-drop-file'
 import subheader from '$lib/app/svelte-stores/subheader'
+import type { Unlistener } from '$lib/types'
 $subheader = undefined
 
-let fileDropUnListener: () => void
+let fileDropUnListener: Unlistener
 
 onMount(async () => {
-  fileDropUnListener = await fileDropListener()
+  fileDropUnListener = await dragDropFileListener()
 })
 
 onDestroy(() => {
