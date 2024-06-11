36b161428dd621b0cd2bea0f23a9e48901953ae6
Karolis Ramanauskas <karolisr@gmail.com>
2024-06-11 18:59:38 -0500

    Basic sequence drawing.

--- src/lib/ui/views/SeqView/SeqView.svelte
+++ src/lib/ui/views/SeqView/SeqView.svelte
@@ -1,10 +1,56 @@
 <script lang="ts">
+import { onMount } from 'svelte'
+import { prepareSiteImages } from '.'
 export let seq: string | undefined = undefined
 export let seqType: string | undefined = undefined
+
+const size = 20
+
+const renderedSites: Map<string, HTMLCanvasElement> = prepareSiteImages(
+  size,
+  `${size}px Monospace`
+)
+let canvas: HTMLCanvasElement | null = null
+let ctx: CanvasRenderingContext2D | null = null
+
+const mar_x = 0
+const mar_y = 0
+const delta_x = size + mar_x
+const delta_y = size + mar_y
+
+function drawSite(label: string, ctx: CanvasRenderingContext2D) {
+  ctx.drawImage(renderedSites.get(label) as HTMLCanvasElement, 0, 0)
+  ctx.translate(delta_x, 0)
+}
+
+function drawSites(
+  ctx: CanvasRenderingContext2D | null,
+  seq: string | undefined
+) {
+  if (ctx !== null && seq !== undefined) {
+    ctx.reset()
+    // ctx.font = '10px JetBrains Mono'
+    // ctx.font = `${size}px JetBrains Mono`
+    for (let i = 0; i < seq.length; i++) {
+      const label = seq[i]
+      drawSite(label, ctx)
+    }
+  }
+}
+
+$: drawSites(ctx, seq)
+
+onMount(() => {
+  canvas = document.getElementById('seqview-canvas') as HTMLCanvasElement
+  ctx = canvas.getContext('2d')
+})
 </script>
 
-<div>{seqType}</div>
-<div>{seq}</div>
+<div class="seqview-container">
+  <!-- <div>{seqType}</div> -->
+  <!-- <div>{seq}</div> -->
+  <canvas id="seqview-canvas"></canvas>
+</div>
 
 <!--
 <div class="seqview-container">
@@ -15,6 +61,26 @@ export let seqType: string | undefined = undefined
 -->
 
 <style>
+.seqview-container {
+  display: flex;
+  flex-direction: column;
+  background-color: azure;
+  overflow-x: scroll;
+  /* margin: 0; */
+  /* font-family: 'JetBrains Mono'; */
+}
+
+#seqview-canvas {
+  flex-grow: 1;
+  flex-shrink: 1;
+  background-color: beige;
+  overflow-x: scroll;
+  /* margin: 0; */
+  /* width: 100px; */
+  /* height: 1000px; */
+  /* font-family: 'JetBrains Mono'; */
+}
+
 /*
 .seqview-container {
   display: flex;
--- src/lib/ui/views/SeqView/index.ts
+++ src/lib/ui/views/SeqView/index.ts
@@ -1,3 +1,68 @@
 import SeqView from './SeqView.svelte'
-
 export default SeqView
+
+export class Point {
+  x: number
+  y: number
+
+  constructor(x: number, y: number) {
+    this.x = x
+    this.y = y
+  }
+}
+
+const color_scheme = new Map([
+  ['a', 'rgba(255,120,120,1)'],
+  ['c', 'rgba(120,120,255,1)'],
+  ['g', 'rgba(251,231,2,1)'],
+  ['t', 'rgba(0,255,0,1)'],
+  ['n', 'rgba(200,200,200,1)'],
+  ['-', 'rgba(255,255,255,1)']
+])
+
+export function calcTextOffset(
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  size: number
+): Point {
+  const m = ctx.measureText(text)
+  const w = m.actualBoundingBoxLeft + m.actualBoundingBoxRight
+  const h = m.actualBoundingBoxAscent + m.actualBoundingBoxDescent
+  const x = (size - w) / 2
+  const y = (size - h) / 2
+  return new Point(x + m.actualBoundingBoxLeft, y + m.actualBoundingBoxAscent)
+}
+
+export function drawSite(
+  ctx: CanvasRenderingContext2D,
+  text: string,
+  size: number
+) {
+  const text_offset = calcTextOffset(ctx, text, size)
+  if (color_scheme.has(text)) {
+    ctx.fillStyle = color_scheme.get(text) as string
+  } else {
+    ctx.fillStyle = '#fff'
+  }
+  ctx.fillRect(0, 0, size, size)
+  ctx.fillStyle = '#000'
+  ctx.fillText(text, text_offset.x, text_offset.y)
+}
+
+export function prepareSiteImages(
+  size: number,
+  font: string
+): Map<string, HTMLCanvasElement> {
+  const renderedSites: Map<string, HTMLCanvasElement> = new Map()
+  for (let [key] of color_scheme) {
+    const buffer = document.createElement('canvas')
+    buffer.width = size
+    buffer.height = size
+    const ctx = buffer.getContext('2d') as CanvasRenderingContext2D
+    // ctx.font = `normal ${size}px ${font}`
+    ctx.font = font
+    drawSite(ctx, key, size)
+    renderedSites.set(key, buffer)
+  }
+  return renderedSites
+}
