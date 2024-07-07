a0a96ca7e2e604fc56737afe8a24cc1304de49f5
Karolis Ramanauskas <karolisr@gmail.com>
2024-07-07 16:24:04 -0500

    Better resource / third-party binary integration.

--- src/lib/app/api/mafft/index.ts
+++ src/lib/app/api/mafft/index.ts
@@ -1,7 +1,7 @@
 import { Command } from '@tauri-apps/plugin-shell'
 
 export async function mafft() {
-  const command = Command.sidecar('sidecar/mafft', ['--help'])
+  const command = Command.create('mafft', ['--help'])
   const output = await command.execute()
   return output
 }
